cba43bf3f6d41199fd836857a4b51358b1911de8
Jucélio Brandão <juceneon@gmail.com>
2024-11-16 22:16:56 -0300

    Adicionar arquivos de projeto.

---
+++ FIAP.IRRIGACAO.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIAP.IRRIGACAO.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
