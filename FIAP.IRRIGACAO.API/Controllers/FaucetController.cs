1dcb01de4e10448b437c941e88dfa02cac74f799
Jucélio Brandão <juceneon@gmail.com>
2024-11-16 23:06:46 -0300

    Cria view de listagem de torneiras e de criação de torneira

---
+++ FIAP.IRRIGACAO.API/Controllers/FaucetController.cs
@@ -0,0 +1,42 @@
+﻿using FIAP.IRRIGACAO.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FIAP.IRRIGACAO.API.Controllers
+{
+    public class FaucetController : Controller
+    {
+        private readonly ILogger<FaucetController> _logger;
+
+        public FaucetController(ILogger<FaucetController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<FaucetModel> faucetList = new List<FaucetModel>();
+            return View(faucetList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Console.WriteLine("Executou a Action Cadastrar()");
+            return View(new FaucetModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(FaucetModel clienteModel)
+        {
+            Console.WriteLine("Gravando a torneira");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FIAP.IRRIGACAO.API/Models/FaucetModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FIAP.IRRIGACAO.API.Models
+{
+    public class FaucetModel
+    {
+        public long Id { get; set; }
+        public bool IsEnabled { get; set; }
+        public long LocationId { get; set; }
+    }
+
+}
